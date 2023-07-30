07fa106c5632b5bdd9069ac18a7d4c4befbf00e7
parul-gogia-325 <113260008+parul-gogia-325@users.noreply.github.com>
2023-07-30 15:20:29 +0530

    Update Degentoken.sol

--- module4/Degentoken.sol
+++ module4/Degentoken.sol
@@ -5,3 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "hardhat/console.sol";
+contract DegenGamingToken is ERC20, Ownable {
+
+    constructor(uint256 initialSupply) ERC20("DegenGamingToken", "DGG") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool success) {
+        return super.transfer(to, amount);
+    }
+
+    function redeem(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function getBalance(address account) public view returns (uint256 balance) {
+        return balanceOf(account);
+    }
+}
