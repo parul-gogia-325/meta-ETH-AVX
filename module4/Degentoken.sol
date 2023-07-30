864871d85fe280e2c6d10b85bc184cb073362db6
parul-gogia-325 <113260008+parul-gogia-325@users.noreply.github.com>
2023-07-30 15:19:46 +0530

    Create Degentoken.sol

---
+++ module4/Degentoken.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
